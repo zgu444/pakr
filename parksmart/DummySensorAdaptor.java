53679363fef4ddf41e74658ef08adbb32345487d
Zilei Gu <zgu444@gmail.com>
2018-09-29 11:13:39 -0400

    sensor adaptor interface

---
+++ parksmart/DummySensorAdaptor.java
@@ -0,0 +1,25 @@
+public class DummySensorAdaptor {
+  private final int x_coor, y_coor;
+  private int const_distance;
+
+  public DummySensorAdaptor(int x, int y, int distance){
+    x_coor = x;
+    y_coor = y;
+    const_distance = distance;
+  }
+
+  @Override
+  public int getDistance(){
+    return const_distance;
+  }
+
+  @Override
+  public int getX(){
+    return x_coor;
+  }
+
+  @Override
+  public int getY(){
+    return y_coor;
+  }
+}
---
+++ parksmart/SensorAdaptor.java
@@ -0,0 +1,12 @@
+public interface SensorAdaptor {
+  
+  /**
+    Get Distance in Centimeters
+  */
+  public int getDistance();
+
+  public int getX();
+
+  public int getY();
+
+}
