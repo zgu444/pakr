c8915b4a4097581dff131df2fb57741dc39e9085
yusihao51 <417824249@qq.com>
2018-10-22 09:13:03 -0400

    impl stream

--- parksmart/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ parksmart/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.myfirstapp;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String RTSP_ADDR = "rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4";// "rtsp://PARKRPI.WV.CC.CMU.EDU:8554/test";
+    private Boolean playing = false;
+    private Boolean URIinit = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+            }
+        });
+
+        Button muteBv = findViewById(R.id.mute);
+        final TextView textV = findViewById(R.id.playStatus);
+        final VideoView videoView = findViewById(R.id.videoViewMain);
+//        videoView.setVideoPath(RTSP_ADDR);
+
+        muteBv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (playing){
+                    videoView.suspend();
+                    playing = false;
+                    textV.setText("Not Playing");
+                }
+                else{
+                    if (true || !URIinit) {
+                        videoView.setVideoURI(Uri.parse(RTSP_ADDR));
+                        videoView.requestFocus();
+                        URIinit = true;
+                    }
+                    videoView.start();
+                    playing = true;
+                    textV.setText("Playing");
+                }
             }
         });
 
