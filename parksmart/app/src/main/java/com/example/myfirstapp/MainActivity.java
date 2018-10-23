17ea559ae0694e7d021c77596c5ae5cc8cef3611
yusihao51 <417824249@qq.com>
2018-10-23 10:45:14 -0400

    add push

--- parksmart/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ parksmart/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.myfirstapp;
 
+import android.graphics.RectF;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String RTSP_ADDR = "rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4";// "rtsp://PARKRPI.WV.CC.CMU.EDU:8554/test";
+    public static final String RTSP_ADDR = "rtsp://PARKRPI.WV.CC.CMU.EDU:8554/test";
     private Boolean playing = false;
     private Boolean URIinit = false;
     @Override
@@ -31,6 +41,23 @@ public class MainActivity extends AppCompatActivity {
         final VideoView videoView = findViewById(R.id.videoViewMain);
 //        videoView.setVideoPath(RTSP_ADDR);
 
+        RequestQueue q = Volley.newRequestQueue(this);
+        StringRequest sr = new StringRequest(Request.Method.GET, "http://www.google.com",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String r) {
+                        Log.d("rtsp string", r);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("rtsp string", error.toString());
+                    }
+                });
+//        q.add(sr);
+        Log.d("rtsp string", "added");
+
         muteBv.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -41,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     if (true || !URIinit) {
-                        videoView.setVideoURI(Uri.parse(RTSP_ADDR));
+                        videoView.setVideoPath((RTSP_ADDR));
+//                        videoView.setVideoURI(Uri.parse(RTSP_ADDR));
                         videoView.requestFocus();
                         URIinit = true;
                     }
